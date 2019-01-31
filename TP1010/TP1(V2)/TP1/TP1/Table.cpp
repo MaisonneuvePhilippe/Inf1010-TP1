96ffabe50bbdec7280eaa309f57b8bd0bf391762
Hugo Perronnet <hugo.perronnet@polymtl.ca>
2019-01-31 18:54:22 -0500

    Modification de Plat, Menu et Table

--- TP1010/TP1(V2)/TP1/TP1/Table.cpp
+++ TP1010/TP1(V2)/TP1/TP1/Table.cpp
@@ -1,28 +1,35 @@
 #include "Table.h"
 #include <fstream>
 
-Table::Table() : capacite_(MAXCAP), id_(-1), nbPlaces_(1), occupee_(false)
+Table::Table() : capacite_(MAXCAP), id_(-1), nbPlaces_(1), occupee_(false), nbPlats_(0)
 {
 	commande_ = new Plat*[MAXCAP];
 }
 
-Table::Table(int id, int nbPlaces) : capacite_(MAXCAP), id_(id), nbPlaces_(nbPlaces), occupee_(false)
+Table::Table(int id, int nbPlaces) : capacite_(MAXCAP), id_(id), nbPlaces_(nbPlaces), occupee_(false), nbPlats_(0)
 {
 	commande_ = new Plat*[MAXCAP];
-
+	
 }
 
-int Table::getId()
+Table::~Table() {
+	for (int i = 0; i < nbPlats_; i++) {
+		commande_[i] = nullptr;
+	}
+	delete[] commande_;
+}
+
+int Table::getId() const
 {
 	return id_;
 }
 
-int Table::getNbPlaces()
+int Table::getNbPlaces() const
 {
 	return nbPlaces_;
 }
 
-bool Table::estOccupee()
+bool Table::estOccupee() const
 {
 	return occupee_;
 }
@@ -64,7 +71,7 @@ double Table::getChiffreAffaire()
 	return prix;
 }
 
-void Table::afficher()
+void Table::afficher() const
 {
 	cout << "La table numero " << id_ << " est ";
 	if (occupee_) {
--- TP1010/TP1(V2)/TP1/TP1/Table.h
+++ TP1010/TP1(V2)/TP1/TP1/Table.h
@@ -15,10 +15,13 @@ public:
 	Table(); 
 	Table(int id, int nbPlaces);
 
+	//Destructeurs
+	~Table();
+
 	//getters 
-	int getId(); 
-	int getNbPlaces(); 
-	bool estOccupee(); 
+	int getId() const;
+	int getNbPlaces() const ; 
+	bool estOccupee() const; 
 
 	//setters 
 	void libererTable(); 
@@ -30,7 +33,7 @@ public:
 	double getChiffreAffaire(); 
 
 	//affichage
-	void afficher(); 
+	void afficher() const; 
 private : 
 	Plat** commande_; 
 	unsigned int capacite_; 
