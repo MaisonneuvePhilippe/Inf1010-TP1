8e33661bec9e316630982d05c85141bfecb53c0b
Hugo Perronnet <hugo.perronnet@polymtl.ca>
2019-02-01 22:33:12 -0500

    Commentaires sur le fichier table.cpp

--- TP1010/TP1(V2)/TP1/TP1/Table.cpp
+++ TP1010/TP1(V2)/TP1/TP1/Table.cpp
@@ -8,16 +8,19 @@
 #include "Table.h"
 #include <fstream>
 
+//Constructeur par défaut
 Table::Table() : capacite_(MAXCAP), id_(-1), nbPlaces_(1), occupee_(false), nbPlats_(0)
 {
 	commande_ = new Plat*[MAXCAP];
 }
 
+//Constructeur
 Table::Table(int id, int nbPlaces) : capacite_(MAXCAP), id_(id), nbPlaces_(nbPlaces), occupee_(false), nbPlats_(0)
 {
 	commande_ = new Plat*[MAXCAP];
 }
 
+//Destructeur
 Table::~Table() {
 	for (int i = 0; i < nbPlats_; i++) {
 		commande_[i] = nullptr;
@@ -25,21 +28,33 @@ Table::~Table() {
 	delete[] commande_;
 }
 
+/*
+* \return id_ de la table
+*/
 int Table::getId() const
 {
 	return id_;
 }
 
+/*
+* \return nombre de places sur un table
+*/
 int Table::getNbPlaces() const
 {
 	return nbPlaces_;
 }
 
+/*
+* \return si la table est occupée ou non
+*/
 bool Table::estOccupee() const
 {
 	return occupee_;
 }
 
+/*
+* La table n'est plus accupée et la commande associée est vidée
+*/
 void Table::libererTable()
 {
 	occupee_ = false;
@@ -50,22 +65,36 @@ void Table::libererTable()
 	}
 }
 
+/*
+* La table devient occupée
+*/
 void Table::placerClient()
 {
 	occupee_ = true;
 }
 
+/*
+* Définit l'id de la table
+*/
 void Table::setId(int id)
 {
 	id_ = id;
 }
 
+/*
+* Ajouter un plat à la commande des clients
+* \param plat : Le plat à ajouter à la commande
+*/
 void Table::commander(Plat * plat)
 {
 	commande_[nbPlats_] = plat;
 	nbPlats_++;
 }
 
+/*
+* Renvoie le chiffre d'affaire de la table
+* \return le chiffre d'affaire
+*/
 double Table::getChiffreAffaire()
 {
 	double prix = 0.0;
@@ -77,6 +106,9 @@ double Table::getChiffreAffaire()
 	return prix;
 }
 
+/*
+* Affiche l'id de la table. Indique si elle est libre ou occupée. Si elle est occupée, affiche les commandes passées par les clients
+*/
 void Table::afficher() const
 {
 	cout << "La table numero " << id_ << " est ";
